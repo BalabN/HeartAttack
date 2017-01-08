53710e1f89ea4caf0b8a2131a8714f878d9aac1f
Nikola Balaban <nikola.balaban1@gmail.com>
2017-01-08 17:56:54 +0100

    Started grid building

---
+++ Assets/Code/Main.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+
+    private float timeToLoad;
+    private bool levelLoaded = false;
+
+    void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Use this for initialization
+    void Start() {
+        timeToLoad = Time.time + 10;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Time.time > timeToLoad && !levelLoaded) {
+            SceneManager.LoadScene("Level1");
+            levelLoaded = true;
+        }
+    }
+}
