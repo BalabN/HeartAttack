0ec0d8c8aeb0f4fc1e2d0cb57a05d60066b84470
Nikola Balaban <nikola.balaban1@gmail.com>
2017-01-21 04:56:55 +0100

    Finished path finding started tower spawning

---
+++ Assets/Code/TowerSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown() {
+        if (Input.GetKey("mouse 0")) {
+            print("Box Clicked!" + gameObject.transform.position.x);
+            GameObject go = (GameObject) Instantiate(Resources.Load("Tower_work_1"));
+            print("Box Clicked!" + gameObject.transform.position.x);
+            go.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 5.6f, gameObject.transform.position.z);
+            print("Box Clicked!" + gameObject.transform.position.x);
+        }
+    }
+}
